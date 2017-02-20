d0c064f3bc3b3313101fbe3e31cc6e4063eecdd7
Chase McCarthy <chase@code0100fun.com>
2017-02-20 00:30:35 -0600

    Fix episode show refresh

--- app/routes/episodes/show.js
+++ app/routes/episodes/show.js
@@ -3,5 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.find('episode', params.episodeId);
-  }
+  },
+  serialize(model) {
+    return { episodeId: model.get('id') };
+  },
 });
