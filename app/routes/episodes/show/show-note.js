300b5f133f92ff6f4933d5a1df8ddf41b1dd4335
Chase McCarthy <chase@code0100fun.com>
2017-02-20 21:05:59 -0600

    Add show-note show page

--- app/routes/episodes/show/show-note.js
+++ app/routes/episodes/show/show-note.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return this.store.findRecord('show-note', params.showNoteId);
+  },
   serialize(model) {
     return { showNoteId: model.get('id') };
   },
