411fc8bf9182bbc4be34cbee36cfac8fc473f7a1
Chase McCarthy <chase@code0100fun.com>
2017-03-26 14:26:05 -0700

    Don't require resource URL

--- app/validations/resource.js
+++ app/validations/resource.js
@@ -3,14 +3,21 @@ import {
   validateLength,
   validateFormat,
 } from 'ember-changeset-validations/validators';
+import or from 'ember-changeset-hofs/utils/or';
+
+function isUndefinedOrNull() {
+  return (key, newValue) => {
+    return typeof(newValue) === 'undefined' || newValue === null || newValue === '';
+  };
+}
 
 export default {
-  url: [
-    validatePresence(true),
-    validateFormat({ type: 'url' }),
-  ],
+  url: or(
+    isUndefinedOrNull(),
+    validateFormat({ type: 'url' })
+  ),
   title: [
     validatePresence(true),
-    validateLength({ min: 4, max: 30 }),
+    validateLength({ min: 4, max: 150 }),
   ],
 };
